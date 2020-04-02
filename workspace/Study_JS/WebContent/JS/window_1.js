76277fabee1d16164f3f2a5c0768cc1ea6edbd2c
playcrabs <yongsin1ok@naver.com>
2020-04-02 17:50:18 +0900

    2020/03/24

---
+++ workspace/Study_JS/WebContent/JS/window_1.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+
+var btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function() {
+	window.open("./window_1_pop.html", "",
+			"width=400, height=200,left=300, top=300");
+});
